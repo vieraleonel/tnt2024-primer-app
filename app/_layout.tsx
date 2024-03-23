91629b2461a85d4fae796853ab96107b47189d65
Leonel Viera <vieraleonel1@gmail.com>
2024-03-22 21:15:14 -0300

    Comenzamos a componentizar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,23 +4,56 @@ import {
   StyleSheet,
   ScrollView,
   useWindowDimensions,
+  FlatList,
 } from "react-native";
 import Contants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { Image } from "expo-image";
+import { Image, ImageBackground, ImageSource } from "expo-image";
 import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FC } from "react";
+
+type Category = {
+  id: number;
+  active: boolean;
+  name: string;
+};
+interface Movie {
+  title: string;
+  image:
+    | string
+    | number
+    | ImageSource
+    | ImageSource[]
+    | string[]
+    | null
+    | undefined;
+}
 
 const orange = "#D98639";
-const categories = [
-  "Action",
-  "Comedy",
-  "Romance",
-  "Thriller",
-  "Fantasy",
-  "Psi-Fi",
-  "Drama",
+const categories: Category[] = [
+  { id: 1, active: true, name: "Action" },
+  { id: 2, active: false, name: "Comedy" },
+  { id: 3, active: false, name: "Romance" },
+  { id: 4, active: false, name: "Thriller" },
+  { id: 5, active: false, name: "Fantasy" },
+  { id: 6, active: false, name: "Psi-Fi" },
+  { id: 7, active: false, name: "Drama" },
+];
+const movies: Movie[] = [
+  {
+    title: "Eternals",
+    image: require("@/assets/images/eternals.png"),
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    image: require("@/assets/images/spidey.png"),
+  },
+  {
+    title: "Matrix",
+    image: require("@/assets/images/matrix.png"),
+  },
 ];
 
 const App = () => {
@@ -53,19 +86,22 @@ const App = () => {
           </View>
         </View>
         <Text style={styles.sectionHeader}>Coming Soon</Text>
-        <Image
+        <ImageBackground
           source={require("@/assets/images/batman.png")}
           style={{
             width: width - 60,
             height: (width - 60) / 1.6,
-            resizeMode: "cover",
             borderRadius: 10,
             justifyContent: "space-between",
             padding: 15,
           }}
+          contentFit="cover"
         >
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
           >
             <Text style={styles.textBase}>The Batman</Text>
             <AntDesign name="sharealt" size={24} color="white" />
@@ -100,108 +136,79 @@ const App = () => {
             />
             <Text style={styles.textBase}>Tickets Available</Text>
           </View>
-        </Image>
+        </ImageBackground>
       </View>
       <View style={{ height: 20 }} />
       <ScrollView horizontal style={{ maxHeight: 30 }}>
-        {categories.map((category, index) => (
-          <View
-            style={
-              index === 0 ? styles.categoryActive : styles.categoryInactive
-            }
-          >
-            <Text
-              style={
-                index === 0
-                  ? styles.categoryActiveText
-                  : styles.categoryInactiveText
-              }
-            >
-              {category}
-            </Text>
-          </View>
+        {categories.map((category) => (
+          <CategoryBadge key={category.id} category={category} />
         ))}
       </ScrollView>
       <View style={{ height: 20 }} />
       <Text style={[styles.sectionHeader, { paddingHorizontal: 30 }]}>
         Now Showing
       </Text>
-      <ScrollView horizontal style={{ maxHeight: 300 }}>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 10,
-            width: 150,
-          }}
-        >
-          <Image
-            source={require("@/assets/images/eternals.png")}
-            style={styles.movieCard}
-          />
-          <Text style={styles.textBase}>Eternals</Text>
-          <View style={{ flexDirection: "row", gap: 10, width: 150 }}>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>13+</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>Action</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>IMAX</Text>
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 10,
-            width: 200,
-          }}
-        >
-          <Image
-            source={require("@/assets/images/spidey.png")}
-            style={styles.movieCard}
-          />
-          <Text style={styles.textBase}>Spider-Man: No Way Home</Text>
-          <View style={{ flexDirection: "row", gap: 10 }}>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>13+</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>Action</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>IMAX</Text>
-            </View>
-          </View>
-        </View>
-        <View
-          style={{ justifyContent: "center", alignItems: "center", gap: 10 }}
-        >
-          <Image
-            source={require("@/assets/images/matrix.png")}
-            style={styles.movieCard}
-          />
-          <Text style={styles.textBase}>Matrix</Text>
-          <View style={{ flexDirection: "row", gap: 10 }}>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>13+</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>Action</Text>
-            </View>
-            <View style={styles.movieCardBadge}>
-              <Text style={styles.movieCardBadgeText}>IMAX</Text>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
+      <FlatList
+        style={{ maxHeight: 300 }}
+        horizontal
+        data={movies}
+        ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
+        renderItem={({ item }) => (
+          <MovieCard title={item.title} image={item.image} />
+        )}
+        keyExtractor={(item) => item.title}
+      />
     </View>
   );
 };
 
+type CategoryBadgeProps = { category: Category };
+const CategoryBadge: FC<CategoryBadgeProps> = ({ category }) => {
+  return (
+    <View
+      key={category.id}
+      style={category.active ? styles.categoryActive : styles.categoryInactive}
+    >
+      <Text style={styles.categoryText}>{category.name}</Text>
+    </View>
+  );
+};
+
+interface MovieCardProps {
+  title: string;
+  image:
+    | string
+    | number
+    | ImageSource
+    | ImageSource[]
+    | string[]
+    | null
+    | undefined;
+}
+const MovieCard = ({ title, image }: MovieCardProps) => (
+  <View
+    style={{
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 10,
+      width: 150,
+    }}
+  >
+    <Image source={image} style={styles.movieCard} />
+    <Text style={styles.textBase}>{title}</Text>
+    <View style={{ flexDirection: "row", gap: 10, width: 150 }}>
+      <View style={styles.movieCardBadge}>
+        <Text style={styles.movieCardBadgeText}>13+</Text>
+      </View>
+      <View style={styles.movieCardBadge}>
+        <Text style={styles.movieCardBadgeText}>Action</Text>
+      </View>
+      <View style={styles.movieCardBadge}>
+        <Text style={styles.movieCardBadgeText}>IMAX</Text>
+      </View>
+    </View>
+  </View>
+);
 const styles = StyleSheet.create({
   textBase: {
     color: "white",
@@ -217,15 +224,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginHorizontal: 5,
   },
-  categoryActiveText: {
-    color: "white",
-    fontSize: 15,
-  },
   categoryInactive: {
     padding: 5,
     marginHorizontal: 5,
   },
-  categoryInactiveText: {
+  categoryText: {
     color: "white",
     fontSize: 15,
   },
